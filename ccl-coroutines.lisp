e228b94e4f39e38cbbaafd3ce324722c57141f34
Shannon Spires <svs@bearlanding.com>
2016-03-10 17:33:25 -0700

    Documentation, exports, packages.

--- ccl-coroutines.lisp
+++ ccl-coroutines.lisp
@@ -1,14 +1,51 @@
-;;; ccl-coroutines.lisp
+;;; ccl-coroutines-basic.lisp
 ;;; Wednesday March 9, 2016
 ;;; Maintained by Shannon Spires <svs@bearlanding.com>
 ;;; Based on code by Gary Byers <gb@clozure.com>
 
-;;; Coroutines for Clozure Common Lisp (OpenMCL)
+;;; Basic coroutines for Clozure Common Lisp (OpenMCL) using fake stack-groups.
 ;;; Tested in CCL 1.11 and later
-
+;;; Limitations of this version:
+;;;   Yield can only return single values.
+;;;   Funcalling a stack-group [coroutine] cannot accept parameters.
+;;;   Probably too verbose. The name "stack-group" everywhere is cumbersome.
 ;;; License: MIT
 
-#|
+#| Comments by SS:
+This creates fake stack-groups in CCL to enable coroutines.
+Each pseudo-stack-group is actually a native OS thread augmented by a semaphore.
+(Early versions of this code used locks, but semaphores are handled by the OS 
+in every implementation of CCL, while locks might not be. Trust me, you want the
+OS scheduler to handle waiting, not userland Lisp code.)
+
+Provisions are made herein to enable ordinary processes to have such semaphores
+so that they can be the resumer of a specialized stack-group process. Thus there's
+no need for the current process to itself be of the stack-group class just to allow
+it to call a stack-group coroutine.
+
+Advantages of this approach:
+It makes it very easy to write coroutines in CCL without changing existing code or
+using special continuation macros. Any function that accepts a function (e.g. the
+Common Lisp mapping functions or the CCL directory function) can be instantly turned
+into a generator or coroutine.
+
+Disadvantages:
+It's not particularly fast. The latency between when a coroutine is ready to return vs.
+when the resumer sees the returned value is dependent on the granularity of the operating
+system's scheduler. That's not usually horrible, but it's not as fast as a native
+stack-group would be.
+
+The returned value is stored in a slot of the producer stack-group process. It's atomically
+protected such that the resumer won't see it half-written by the producer, but this
+code doesn't support having more than one resumer for a given producer, or the return
+of multiple values from a stack-group (although multiple values could probably be supported 
+fairly easily).
+
+It's not possible in this code for the resumer to call the stack-group with a parameter.
+Many coroutine algorithms require this. It's probably not too difficult to add. 
+|#
+
+#| Comments by gb:
 FUNCALL-STACK-GROUP makes the calling thread be the resumer of the target stack group,
 then waits until the target returns a value via STACK-GROUP-RETURN. I don't see how
 it makes sense for a stack group to have more than one resumer at any time, so the
@@ -28,16 +65,20 @@ that it could allow mutual recursion (A calls B which calls A, etc.), and I can 
 of lots of fairly good reasons for not supporting it.
 
 If a stack group dies prematurely then some state (in it and the resumer) would need
-to be cleaned up. I haven't done anything about this (or even thought about it in any
-detail.)
+to be cleaned up. There may be more work to do here.
 |#
 
 (in-package :ccl)
 
+(export '(:stack-group-return
+          :make-stack-group
+          :funcall-stack-group
+          :stack-group-preset))
+
 (defclass stack-group (process)
   ((%resumer :initform (vector nil))
    (value :initform nil)
-   (run :initform (make-semaphore) :reader stack-group-runsem)))
+   (run :initform (make-semaphore) :reader stack-group-semaphore)))
 
 (define-condition stack-group-exhausted (error)
   ((stack-group :initarg :stack-group)
@@ -61,7 +102,7 @@ detail.)
       (error "Can't set ~s of ~s to ~s."
              'stack-group-resumer sg new))))
 
-(defmethod stack-group-runsem ((p process))
+(defmethod stack-group-semaphore ((p process))
   (or (getf (process-plist p) :run)
       (setf (getf (process-plist p) :run) (make-semaphore))))
 
@@ -81,7 +122,7 @@ detail.)
   (make-process name :class 'stack-group))
 
 (defun wait-to-run ()
-  (wait-on-semaphore (stack-group-runsem *current-process*)))
+  (wait-on-semaphore (stack-group-semaphore *current-process*)))
 
 (defmethod stack-group-preset ((sg stack-group) function &rest args)
   (process-preset sg (lambda ()
@@ -95,7 +136,7 @@ detail.)
   (when (process-exhausted-p sg)
     (error 'stack-group-exhausted :stack-group sg :calling-thread *current-process*))
   (setf (stack-group-resumer sg) *current-process*)
-  (signal-semaphore (stack-group-runsem sg))
+  (signal-semaphore (stack-group-semaphore sg))
   (wait-to-run)
   (stack-group-value sg))
 
@@ -103,73 +144,88 @@ detail.)
   (let* ((self *current-process*)
          (resumer (stack-group-resumer self)))
     (setf (stack-group-value self) value)
-    (signal-semaphore (stack-group-runsem resumer))))
+    (signal-semaphore (stack-group-semaphore resumer))))
 
 (defun stack-group-return (value)
+  "Returns value to resumer. Returned value from THIS function is always T."
   (%stack-group-return value)
   (wait-to-run))
 
-(defmacro yield (value)
-  "Conventional coroutine terminology"
+(in-package :cl-user)
+
+(defmacro yield (value) ; similar name defined in ccl but fortunately not exported.
+  "Conventional coroutine terminology."
   `(stack-group-return ,value))
 
-#| Example 1. Samefringe.
-(defun fringe1 (tree exhausted)
-  (labels ((fringe (tree)
-             (if (atom tree)
-               (stack-group-return tree)
-               (progn
-                 (fringe (car tree))
-                 (when (cdr tree) (fringe (cdr tree)))))))
-    (fringe tree)
-    exhausted))
+; Example 1. Samefringe.
+#+EXAMPLE
+(progn
+  (defun fringe1 (tree exhausted)
+    (labels ((fringe (tree)
+               (if (atom tree)
+                   (stack-group-return tree)
+                   (progn
+                     (fringe (car tree))
+                     (when (cdr tree) (fringe (cdr tree)))))))
+      (fringe tree)
+      exhausted))
+  
+  (defun samefringe (tree1 tree2)
+    (let* ((sg1 (make-stack-group "sg1"))
+           (sg2 (make-stack-group "sg2"))
+           (done (cons nil nil)))
+      (stack-group-preset sg1 #'fringe1 tree1 done)
+      (stack-group-preset sg2 #'fringe1 tree2 done)
+      (loop
+        (let* ((v1 (funcall-stack-group sg1))
+               (v2 (funcall-stack-group sg2)))
+          (cond ((not (eq v1 v2)) (return nil))
+                ((eq v1 done) (return t)))))))
+  
+  (samefringe '(a b c) '(a (b c)))
+  )
 
-(defun samefringe (tree1 tree2)
-  (let* ((sg1 (make-stack-group "sg1"))
-         (sg2 (make-stack-group "sg2"))
-         (done (cons nil nil)))
-    (stack-group-preset sg1 #'fringe1 tree1 done)
-    (stack-group-preset sg2 #'fringe1 tree2 done)
-    (loop
-      (let* ((v1 (funcall-stack-group sg1))
-             (v2 (funcall-stack-group sg2)))
-        (cond ((not (eq v1 v2)) (return nil))
-              ((eq v1 done) (return t)))))))
+; Example 2. Create a generator by using mapc.
+#+EXAMPLE
+(progn
+  (defun gen (items)
+    (mapc (lambda (item) (yield item)) items)
+    nil ; force last value to be nil
+    )
+  
+  (defun make-generator (sg items)
+    (stack-group-preset sg #'gen items)
+    (lambda ()
+      (funcall-stack-group sg)))
+  )
 
-(samefringe '(a b c) '(a (b c)))
-
-|#
-
-#| Example 2. Convert mapcar into a generator.
-(defun gen (items)
-  (mapcar (lambda (item) (yield item)) items))
-
-(defun make-generator (sg items)
-  (stack-group-preset sg #'gen items)
-  (lambda ()
-    (funcall-stack-group sg)))
-
-; (setf c (make-generator (make-stack-group "bar") '(a b c d e f)))
-; (funcall c) -> A
-; (funcall c) -> B
+; (setf g (make-generator (make-stack-group "bar") '(a b c d e f)))
+; (funcall g) -> A
+; (funcall g) -> B
 ; etc.
+; Pay attention to when it returns nil. That means it's done. If you call it
+;   after that, it'll throw a stack-group-exhausted error.
 
-|#
-
-#| Example 3. A quick-and-dirty directory generator. 
-
-(defun my-directory (dirpath)
-  (directory dirpath :test #'(lambda (item) (yield item) nil)))
-
-(defun directory-generator (dirpath)
-  (let ((sg-dir (make-stack-group "dir")))
-    (stack-group-preset sg-dir #'my-directory dirpath)
-    #'(lambda ()
-        (funcall-stack-group sg-dir))))
+; Example 3. A quick-and-dirty directory generator. 
+#+EXAMPLE
+(progn
+  (defun my-directory (dirpath)
+    (directory dirpath :test #'(lambda (item) (yield item) nil))
+    ; nil inside lambda above prevents #'directory from returning the found file as
+    ;  one of its results. In fact, we want #'directory to return nil itself as a signal
+    ;  that the generator is finished.
+    )
+  
+  (defun directory-generator (dirpath)
+    (let ((sg-dir (make-stack-group "dir")))
+      (stack-group-preset sg-dir #'my-directory dirpath)
+      #'(lambda ()
+          (funcall-stack-group sg-dir))))
+  )
 
 ;(setf g (directory-generator "ccl:*"))
 ;(funcall g)
 ;(funcall g)
 ; etc.
-
-|#
+; Pay attention to when it returns nil. That means it's done. If you call it
+;   after that, it'll throw a stack-group-exhausted error.
